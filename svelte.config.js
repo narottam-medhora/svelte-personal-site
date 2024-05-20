9a592a57b3155a7f8e1b4a61b1a0c9d1530c7be0
Narottam Medhora <narottammedhora@gmail.com>
2024-05-20 17:17:14 +0100

    Adds syntax highlighting with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-import { mdsvex } from 'mdsvex'
+import { escapeSvelte, mdsvex } from 'mdsvex'
+import { getHighlighter } from 'shiki'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['dracula'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'dracula' }))
+			return `{@html \`${html}\` }`
+		}
+	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
