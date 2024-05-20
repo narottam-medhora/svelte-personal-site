32843484047e0eb106775ffba9718a34327fbfff
Narottam Medhora <narottammedhora@gmail.com>
2024-05-20 11:06:10 +0100

    Creates default project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,2 @@
-<script>
-	import { siteTitle } from '$lib/config';
-</script>
-
-<svelte:head>
-	<title>{siteTitle}</title>
-</svelte:head>
-
-<h1>Hi, I'm Narottam</h1>
-<p>I'm a data visualisation developer, journalist, and newbie triathlete 🏃🏼🏊🏼‍♂️🚴🏼‍♂️</p>
-<p>
-	I live in South West London and work for
-	<a href="https://www.thetimes.co.uk/" alt="The Times">The Times</a>
-	as a data and digital graphics developer.
-</p>
-<p>
-	Most of the site is undergoing a major redevelopment but we should be up and running shortly! 🚀
-</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,47 +1,17 @@
-import adapter from '@sveltejs/adapter-static'
-import { mdsvex } from 'mdsvex'
-import preprocess from 'svelte-preprocess'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
-	extensions: ['.svelte', '.md'],
-
-	preprocess: [
-		preprocess({
-			scss: {
-				// Ensures Sass variables are always available inside component <style> blocks as vars.$variableDefinedInFile
-				prependData: `@use 'src/lib/assets/scss/vars';`
-			},
-		}),
-		mdsvex({
-			// The default mdsvex extension is .svx; this overrides that.
-			extensions: ['.md'],
-
-			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
-	],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-    prerender: {
-      entries: [
-        '*',
-        '/api/posts/page/*',
-        '/blog/category/*/page/',
-        '/blog/category/*/page/*',
-        '/blog/category/page/',
-        '/blog/category/page/*',
-        '/blog/page/',
-        '/blog/page/*',
-      ]
-    }
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
