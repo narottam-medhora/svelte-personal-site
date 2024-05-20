404d283612b2ca856c7bfa16149281091c4bace8
Narottam Medhora <narottammedhora@gmail.com>
2024-05-20 15:49:17 +0100

    Shows posts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,6 +3,7 @@ export type Categories = 'sveltekit' | 'svelte';
 export type Post = {
     title: string,
     date: string,
+    description: string,
     categories: Categories[],
     slug: string,
     published: boolean
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data
+
+	import * as config from '$lib/config'
+	import { formatDate } from '$lib/utils'
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={post.slug} class="title">
+					{post.title}
+				</a>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	.posts {
+		display: grid;
+		gap: var(--size-7);
+	}
+
+	.posts {
+		max-inline-size: var(--size-content-3);
+	}
+
+	.post:not(:last-child) {
+		border-block-end: 1px solid var(--border);
+		padding-block-end: var(--size-7);
+	}
+
+	.title {
+		font-family: var(--font-serif), serif;
+		font-size: var(--font-size-fluid-3);
+	}
+
+	.date {
+		font-family: var(--font-sans), sans-serif;
+	}
+
+	.description {
+		margin-block-start: var(--size-3);
+	}
+</style>
